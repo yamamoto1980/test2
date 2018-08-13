64f680ac8a4aea1a80f7384b27d9d57830e4d5a8
yamamoto <yamamoto@solmarenomac-mini.local>
2018-08-13 15:15:24 +0900

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by yamamoto on 2018/08/13.
+//  Copyright © 2018年 yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by yamamoto on 2018/08/13.
+//  Copyright © 2018年 yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
